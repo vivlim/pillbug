03b8484e8083aab64c0851695dae02fd832a2757
Viv Lim <git@vvn.space>
2024-09-22 23:16:02 -0700

    able to successfully get token.

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,32 @@ import { createStore, SetStoreFunction } from "solid-js/store";
 import { RouteProps, RouteSectionProps } from "@solidjs/router";
 import OAuth from "megalodon/lib/src/oauth";
 import { makePersisted } from "@solid-primitives/storage";
+import generator, { MegalodonInterface } from "megalodon";
 
 const AuthContext = createContext<AuthProviderProps>();
 
 export interface AuthState {
-    appData: OAuth.AppData | undefined;
-    instanceUrl: string | undefined;
+    appData?: OAuth.AppData | undefined;
+    instanceUrl?: string | undefined;
+    instanceSoftware?:
+        | "mastodon"
+        | "pleroma"
+        | "friendica"
+        | "firefish"
+        | "gotosocial"
+        | undefined;
+    authorizationCode?: string | undefined;
+    token?: TokenState | undefined;
 }
+
+interface TokenState {
+    tokenData: OAuth.TokenData;
+    expiresAfterTime: number | null;
+}
+
+export class GetClientError extends Error {}
+
+export const AppDisplayName: string = "pillbug";
 
 interface AuthProviderProps {
     authState: AuthState;
@@ -26,6 +45,195 @@ export function useAuthContext(): AuthProviderProps {
         throw new Error("useAuthContext must be used within a provider");
     }
     return value;
+}
+
+function tokenIsExpired(tokenState: TokenState): boolean {
+    const nowUtc: number = new Date().getTime();
+    if (
+        tokenState.expiresAfterTime !== null &&
+        nowUtc >= tokenState.expiresAfterTime
+    ) {
+        return true;
+    }
+    return false;
+}
+
+async function checkToken(
+    authContext: AuthProviderProps,
+    tryRefresh: boolean
+): Promise<boolean> {
+    let tokenState = authContext.authState.token;
+
+    if (tokenState === undefined) {
+        console.log("There is no token state currently");
+        return false;
+    }
+
+    if (tokenIsExpired(tokenState)) {
+        // Expired.
+        console.log("Token is expired.");
+        if (tryRefresh) {
+            if (tokenState.tokenData.refresh_token === null) {
+                console.log("Token expired and we don't have a refresh token.");
+                return false;
+            }
+
+            try {
+                await tryGetToken(authContext);
+            } catch (error) {
+                if (error instanceof Error) {
+                    console.log(
+                        `Failed to get token using refresh token: ${error.message}`
+                    );
+                    return false;
+                }
+            }
+
+            return await checkToken(authContext, false);
+        } else {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+export async function tryGetAuthenticatedClient(
+    authContext: AuthProviderProps
+): Promise<MegalodonInterface | null> {
+    let authState = authContext.authState;
+
+    if (
+        authState.instanceSoftware === undefined ||
+        authState.instanceUrl === undefined ||
+        authState.token === undefined
+    ) {
+        console.log(
+            "Can't get an authenticated client, auth state isn't initialized"
+        );
+        return null;
+    }
+
+    try {
+        let token = await tryGetToken(authContext);
+
+        let client = generator(
+            authState.instanceSoftware,
+            authState.instanceUrl,
+            authState.token.tokenData.access_token,
+            AppDisplayName
+        );
+        return client;
+    } catch (error) {
+        if (error instanceof Error) {
+            console.log(`Failed to get authenticated client. ${error.message}`);
+        }
+        return null;
+    }
+}
+
+export function tryGetUnauthenticatedClient(
+    authContext: AuthProviderProps
+): MegalodonInterface {
+    let authState = authContext.authState;
+
+    if (
+        authState.instanceSoftware === undefined ||
+        authState.instanceUrl === undefined
+    ) {
+        throw new GetClientError(
+            "Instance software and/or url were not defined in auth state."
+        );
+    }
+    let client = generator(authState.instanceSoftware, authState.instanceUrl);
+    return client;
+}
+
+/// Try to get an access token. Throws if there are problems.
+/// If we have one which isn't expired, that will be used.
+/// If it is expired, and we have a refresh token, we'll try to use that.
+/// If there is no token then try to get one.
+export async function tryGetToken(
+    authContext: AuthProviderProps
+): Promise<OAuth.TokenData> {
+    let authState = authContext.authState;
+    let client = tryGetUnauthenticatedClient(authContext);
+
+    let tokenDataPromise: Promise<OAuth.TokenData> | null = null;
+
+    const nowUtc: number = new Date().getTime();
+
+    // Do we have a token now? Maybe we can use it.
+    if (authState.token !== undefined) {
+        console.log("there is currently a token");
+        if (tokenIsExpired(authState.token)) {
+            if (authState.token.tokenData.refresh_token === null) {
+                throw new GetClientError(
+                    "Token is expired and we have no refresh token."
+                );
+            }
+
+            if (
+                authState.appData === undefined ||
+                authState.appData?.client_id === undefined ||
+                authState.appData.client_secret === undefined
+            ) {
+                throw new GetClientError(
+                    `Missing client id / secret, cannot use refresh token.`
+                );
+            }
+
+            // It's expired and we have a refresh token, let's use it.
+            tokenDataPromise = client.refreshToken(
+                authState.appData.client_id,
+                authState.appData.client_secret,
+                authState.token.tokenData.refresh_token
+            );
+        } else {
+            // token is not expired.
+            return authState.token.tokenData;
+        }
+    } else {
+        // We don't have a token, so let's try to get one.
+        console.log("there is currently no token");
+
+        if (
+            authState.appData === undefined ||
+            authState.appData.redirect_uri === null
+        ) {
+            // this shouldn't happen, tryGetClient() should throw instead
+            throw new GetClientError(
+                "App registration data isn't set, which is unexpected"
+            );
+        }
+        if (authState.authorizationCode === undefined) {
+            throw new GetClientError(
+                "Authorization code isn't set, can't get a token"
+            );
+        }
+        tokenDataPromise = client.fetchAccessToken(
+            authState.appData.client_id,
+            authState.appData.client_secret,
+            authState.authorizationCode,
+            authState.appData.redirect_uri
+        );
+    }
+
+    let accessToken = await tokenDataPromise;
+
+    if (accessToken.expires_in === null) {
+        authContext.setAuthState("token", {
+            tokenData: accessToken,
+            expiresAfterTime: null,
+        });
+    } else {
+        authContext.setAuthState("token", {
+            tokenData: accessToken,
+            expiresAfterTime: nowUtc + accessToken.expires_in,
+        });
+    }
+
+    return accessToken;
 }
 
 const App: Component<RouteSectionProps> = (props: RouteSectionProps) => {
--- src/views/login.tsx
+++ src/views/login.tsx
@@ -2,7 +2,13 @@ import { makePersisted } from "@solid-primitives/storage";
 import { useLocation, useSearchParams } from "@solidjs/router";
 import generator, { detector } from "megalodon";
 import { createResource, createSignal, type Component } from "solid-js";
-import { useAuthContext } from "~/App";
+import {
+    GetClientError,
+    tryGetUnauthenticatedClient,
+    tryGetToken,
+    useAuthContext,
+    AppDisplayName,
+} from "~/App";
 import { NewInstanceOauth } from "~/client/auth";
 import { Button } from "~/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
@@ -20,15 +26,33 @@ const LoginView: Component = () => {
     const [searchParams, setSearchParams] = useSearchParams();
 
     const doOAuth = async () => {
+        if (busy()) {
+            console.log(
+                "tried to enter oauth when already busy. taking no action"
+            );
+            return;
+        }
         setBusy(true);
         try {
             let software = await detector(instance());
+            console.log(`detected software '${software}' on ${instance()}`);
+
+            // Set instance url and software in auth state. That's the minimum needed to get client
             authContext.setAuthState("instanceUrl", (_) => instance()); // Replace any existing app data. Probably shouldn't be able to do this if you are signed in!
-            let client = generator(software, instance());
+            authContext.setAuthState("instanceSoftware", (_) => software);
+
+            let client = tryGetUnauthenticatedClient(authContext);
+
             let redirect_uri = window.location.href;
+            if (window.location.search.length > 0) {
+                redirect_uri = redirect_uri.substring(
+                    0,
+                    redirect_uri.length - window.location.search.length
+                );
+            }
             console.log(`redirect uri: ${redirect_uri}`);
 
-            let appData = await client.registerApp("pillbug", {
+            let appData = await client.registerApp(AppDisplayName, {
                 redirect_uris: redirect_uri, // code will be passed as get parameter 'code'
             });
             if (appData === undefined || appData.url === null) {
@@ -37,7 +61,6 @@ const LoginView: Component = () => {
                 return;
             }
             authContext.setAuthState("appData", (_) => appData); // Replace any existing app data. Probably shouldn't be able to do this if you are signed in!
-            console.log(JSON.stringify(authContext));
             window.location.assign(appData.url);
         } catch (error) {
             if (error instanceof Error) {
@@ -54,13 +77,38 @@ const LoginView: Component = () => {
     const [error, setError] = createSignal<string | undefined>(undefined);
     //const [appData] = createResource(instance, doOAuth);
 
-    if (searchParams.code !== undefined) {
-        setBusy(true);
-        // test instanceurl
-        console.log(
-            `entered login for instance ${authContext.authState.instanceUrl} with a code`
-        );
-    }
+    const getToken = async () => {
+        if (searchParams.code !== undefined) {
+            if (busy()) {
+                console.log(
+                    "tried to enter getting token when already busy. taking no action"
+                );
+                return;
+            }
+            setBusy(true);
+            try {
+                console.log(
+                    "trying to acquire a token using the provided code"
+                );
+
+                let authContext = useAuthContext();
+                authContext.setAuthState(
+                    "authorizationCode",
+                    searchParams.code
+                );
+                await tryGetToken(authContext);
+                console.log("got token!");
+                setBusy(false);
+            } catch (error) {
+                if (error instanceof Error) {
+                    console.log(`error getting token ${error.message}`);
+                    setError(error.message);
+                    setBusy(false);
+                }
+            }
+        }
+    };
+    getToken();
 
     return (
         <div class="flex flex-row p-8 size-full">
