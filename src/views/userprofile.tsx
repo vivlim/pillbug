e3db0139e54529c4cf2c81f948e60543635c7ef8
snow flurry <snow@datagirl.xyz>
2024-10-08 15:19:10 -0700

    Add PostFeed to userprofile view

--- src/views/userprofile.tsx
+++ src/views/userprofile.tsx
@@ -3,18 +3,14 @@ import { Entity } from "megalodon";
 import { createResource, createSignal, Show, type Component } from "solid-js";
 import { AuthProviderProps, useAuthContext } from "~/lib/auth-context";
 import { ProfileZone } from "~/components/user/profile-zone";
+import { GetTimelineOptions, PostFeed } from "~/components/post/feed";
 
-type FeedProps = {
-    firstPost?: number | null;
-};
-
-type GetTimelineOptions = {
-    local?: boolean;
-    limit?: number;
-    max_id?: string;
-    since_id?: string;
-    min_id?: string;
-};
+interface GetAccountFeedOptions extends Omit<GetTimelineOptions, "local"> {
+    pinned?: boolean;
+    exclude_replies?: boolean;
+    exclude_reblogs?: boolean;
+    only_media?: boolean;
+}
 
 const fetchUserInfo = async (
     authContext: AuthProviderProps,
@@ -49,7 +45,29 @@ const UserProfile: Component = () => {
         >
             <div class="flex flex-col md:flex-row mx-1 md:mx-4 gap-4">
                 <ProfileZone userInfo={userInfo()!} />
-                <div class="grow flex flex-col justify-start"></div>
+                <div class="grow flex flex-col justify-start">
+                    <PostFeed
+                        onRequest={(authContext, timelineOptions) => {
+                            const acctFeedProps: GetAccountFeedOptions = {
+                                exclude_replies: true,
+                                ...timelineOptions,
+                            };
+                            if (!authContext.authState.signedIn) {
+                                return;
+                            }
+
+                            // TODO: pinned statuses, somehow
+
+                            const client =
+                                authContext.authState.signedIn
+                                    .authenticatedClient;
+                            return client.getAccountStatuses(
+                                userInfo()!.id,
+                                acctFeedProps
+                            );
+                        }}
+                    />
+                </div>
             </div>
         </Show>
     );
