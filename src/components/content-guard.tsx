2751d30011ac0a943f0b54e9f3c3ebae7b0c2d3e
Viv Lim <git@vvn.space>
2024-10-13 02:49:16 -0700

    add a bit of padding back to content guard

--- src/components/content-guard.tsx
+++ src/components/content-guard.tsx
@@ -13,7 +13,7 @@ export const ContentGuard: Component<ContentGuardProps> = (props) => {
     return (
         <Switch fallback={props.children}>
             <Match when={props.warnings.trim().length > 0}>
-                <div class="flex flex-row gap-2 items-center">
+                <div class="flex flex-row gap-2 items-center p-2">
                     <div class="flex flex-row items-center gap-2 flex-grow p-2 border bg-amber-200 dark:bg-yellow-950 border-yellow-800 dark:border-amber-500 rounded-sm text-amber-950 dark:text-amber-200">
                         <IoWarningOutline class="size-5" />
                         <p>
