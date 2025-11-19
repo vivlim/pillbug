0537837d31289caa9f9e40a3b5e60769ba0cd9e1
Viv Lim <git@vvn.space>
2025-11-19 10:40:08 -0800

    Revert "Add loading spinner to feed"
    This reverts commit 4551a241482bec93e2b23c7fee9a61984227f233.

--- src/components/feed/index.tsx
+++ src/components/feed/index.tsx
@@ -31,7 +31,6 @@ import { createStore } from "solid-js/store";
 import { PageNav } from "../ui/page-footer";
 import { useSettings } from "~/lib/settings-manager";
 import { logger } from "~/logging";
-import { PillbugLoadingSpinner } from "../loading-spinner";
 
 export type FeedComponentProps = {
     rules: FeedRuleProperties[];
@@ -251,9 +250,6 @@ export const FeedComponentPostList: Component<{
                             </Button>
                         </PageNav>
                     </Show>
-                </Match>
-                <Match when={posts.loading}>
-                    <PillbugLoadingSpinner label="loading posts..." />
                 </Match>
             </Switch>
             <Show when={settings.getPersistent().enableDevTools}>
